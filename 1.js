a7966d747940989494dcbf6d718931ce506c2ab9
QuyLX <lexuanquylxq@gmail.com>
2020-10-01 09:39:43 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,23 @@
+﻿$(function(){
+	//khai bao khoi su dung isotope
+	//
+	khoimonan = $('.nhieumon').isotope({
+	  itemSelector: '.motmon',
+	  layoutMode: 'masonry'
+	});
+	// load anh xong ms chay hieu ung
+	khoimonan.imagesLoaded().progress( function() {
+
+	khoimonan.isotope('layout');
+	
+	});
+	//click vao link lay du lieu
+	//sau do load
+	$('.tieudect a').click(function(){
+		dulieu = $(this).data('monan');
+		console.log(dulieu);
+		khoimonan.isotope({ filter: dulieu })
+
+		return false;
+	});
+})
